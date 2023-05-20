ed2ae8dc8e143a94557db12aef0628c84dd1fb7b
Lucas Farias <109489204+FILElucas99@users.noreply.github.com>
2023-05-20 20:55:04 -0300

    adicionando estruturas

--- Gerenciador_de_contas/lib.h
+++ Gerenciador_de_contas/lib.h
@@ -3,6 +3,38 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
+#define tam 50
+
+typedef struct cartao Cartao;
+
+typedef struct usuario {
+    char nome[100];
+    int cpf;
+    char endereco[50];
+    Cartao *cartoes[tam];
+} Usuario;
+
+typedef struct compra {
+    char itens[50];
+    float valor;
+    Usuario user;
+} Compra;
+
+typedef struct arvcompras {
+    Compra compras;
+    int fator;
+    struct arvcompras *esq, *dir;
+} ArvCompras;
+
+struct cartao {
+    int num;
+    Usuario titular;
+    char validade[10];
+    char bandeira[10];
+    int codigo;
+    ArvCompras *dados;
+};
+
 
 
 
